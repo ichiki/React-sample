0e3d28807a8196673ef5e3106caa80828579c14b
Ichiki Yamasato <iyamasat@yahoo-corp.jp>
2017-08-16 10:37:57 +0900

    add chat server

---
+++ chat/chat-server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const portNo = 3001
+server.listen(portNo, ()=>{
+  console.log('start server', 'http://localhost:' + portNo)
+})
+
+app.user('/public', express.static('./public'))
+app.get('/', (req, res) => {
+  res.redirect(302, '/public')
+})
+
+const socketio = require('socket.io')
+const io = socketio.listen(server)
+
+io.on('connection', (socket) => {
+  console.log('connection', socket.client.id)
+  socket.on('chat-msg', (msg) => {
+    console.log('message', msg)
+    io.emit('chat-msg', msg)
+  })
+})
